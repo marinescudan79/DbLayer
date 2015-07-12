e7a659cae3722c713c80ec78a6043d66f7c3737f
Dan Marinescu <dan.marinescu79@gmail.com>
2015-07-12 06:28:12 +0300

    passing object

--- src/DbLayer/Service/DbLayerService.php
+++ src/DbLayer/Service/DbLayerService.php
@@ -5,7 +5,7 @@
  * @Email: dan.m@my1hr.com
  * @Date:   2015-07-11 02:56:38
  * @Last Modified by:   Dan Marinescu
- * @Last Modified time: 2015-07-11 21:55:23
+ * @Last Modified time: 2015-07-12 06:27:57
  */
 
 namespace DbLayer\Service;
@@ -20,6 +20,6 @@ class DbLayerService implements ServiceLocatorAwareInterface
     public function get($value)
     {
         $adapter = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
-        return new \DbLayer\Service\TableGateway($value, $adapter);
+        return new \DbLayer\Service\TableGateway($value, (object) $adapter);
     }
 }
